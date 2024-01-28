7f428a89fab7eb6d90072d40bb192f2f05a895c5
acer <acer@LAPTOP-H9QES7A9>
2024-01-28 18:36:39 +0100

    Aggiungere i file di progetto.

---
+++ Frazione/Frazione.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frazione
+{
+    internal class Frazione
+    {
+        public int Numeratore { get; set; }
+        public int Denominatore { get; set; }
+
+        public Frazione(int numeratore, int denominatore) 
+        {
+            SemplificaFrazione(ref numeratore, ref denominatore);
+
+            Numeratore = numeratore;
+            Denominatore = denominatore;
+        }
+
+        private void SemplificaFrazione(ref int numeratore, ref int denominatore)
+        {
+            int  mcd = TrovaMCD(numeratore, denominatore);
+            numeratore /= mcd;
+            denominatore /= mcd;
+        }
+
+        private int TrovaMCD(int n1, int n2)
+        {
+            while (n2 != 0)
+            {
+                int t = n2;
+                n2 = n1 % n2;
+                n1 = t;
+            }
+             return n1;
+        }
+
+        public override string ToString()
+        {
+            return "(numeratore)/(denominatore)";
+        }
+
+        public static Frazione Parse(string s)
+        {
+            string[] parts = s.Split('/');
+            int Numeratore = int.Parse(parts[0]);
+            int Denominatore = int.Parse(parts[1]);
+
+            return new Frazione(Numeratore, Denominatore);
+        }
+    }
+}
---
+++ Frazione/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frazione
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Frazione frazione = new Frazione(4, 9);
+            Console.WriteLine(frazione.ToString());
+            string a = Console.ReadLine();
+            Frazione frazione2 = Frazione.Parse(a); 
+            Console.WriteLine(frazione2.ToString());
+            
+            Console.ReadLine();
+        }
+    }
+}
